c61f107813bde6ad5185b3dc07d27a6d2d995861
ThearyanThe <aryan11228sh@gmail.com>
2024-10-07 11:13:21 -0700

    notif model

---
+++ model/Notif/Notif.js
@@ -0,0 +1,15 @@
+
+
+  const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const NotificationSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    message: { type: String, required: true },
+    read: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now }
+  },{timestamps:true})
+
+
+//compile schema to model
+const Notif=mongoose.model("Notif",NotificationSchema)
+module.exports=Notif
